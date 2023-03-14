6560c8dd767ef2493e4fa28ce691fb1b87f64202
Shawn <s_wagoner85@yahoo.com>
2023-03-14 14:09:45 -0500

    snakes

---
+++ week 5/mon.js
@@ -0,0 +1,87 @@
+/*  let arr1 = [1, 10, 5];
+  let arr2 = [6, 2, 8];
+  arr1 = [...arr1, ...arr2];
+ console.log(arr1)
+ let newArr = arr1;
+ arr1.sort((a,b) => {
+    return a - b;
+ })
+
+ console.log(arr1);
+*/
+
+/*  class Animal {
+    constructor (name){
+        this.n=name;
+        
+       }
+    speak(){
+        console.log(`${this.n} makes a noise.`);
+    }
+}
+
+   class Cat extends Animal{
+    speak(){
+        super.speak();
+        console.log(`${this.n} meows.`)
+    }
+   }
+
+   class Cheeta extends Cat{
+    speak(){
+        super.speak();
+        console.log(`${this.n} is fast.`)
+    }
+   }
+
+   class Panther extends Cat{
+    speak(){
+        super.speak();
+        console.log(`${this.n} stalks its prey`)
+    }
+   }
+  
+   const one = new Cat("The Kitten");
+   one.speak();
+
+   const two = new Cheeta("This lil kitty");
+   two.speak();
+
+   const three= new Panther("The hunter");
+    three.speak();
+
+    class Snake extends Animal{
+        speak(){
+            super.speak();
+            console.log(`${this.n} is a dangerous reptile.`)
+        }
+       }
+    
+       class Viper extends Snake{
+        speak(){
+            super.speak();
+            console.log(`The ${this.n} strikes fast.`)
+        }
+       }
+    
+       class DiamondBack extends Snake{
+        speak(){
+            super.speak();
+            console.log(`The ${this.n} kills the quickest`)
+        }
+       }
+      
+       const four = new Snake("The Rattle snake");
+       four.speak();
+    
+       const five = new Viper("The Mamba");
+       five.speak();
+    
+       const six= new DiamondBack("Cobra");
+        six.speak();
+*/
+
+
+
+
+    
